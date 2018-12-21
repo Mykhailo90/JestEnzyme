e132cbddd9d381a4ececdc6228605552ef1af822
msarapii <mykhailosarapii@gmail.com>
2018-12-21 16:48:32 +0200

    2 commit

--- demo/src/App.test.js
+++ demo/src/App.test.js
@@ -1,9 +1,5 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
 
 test('renders without error', () => {
-  const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
-  // ReactDOM.unmountComponentAtNode(div);
+
+  
 });
